d923d5c77b04f0465bd63353f9819cf62601e51c
Adam Price <aesprice@gmail.com>
2014-08-05 15:14:35 -0700

    refactored libraryView into its own ShelfView class

--- www/src/main.js
+++ www/src/main.js
@@ -17,8 +17,9 @@ define(function(require, exports, module) {
     var ScrollView   = require('famous/views/ScrollView');
     var Surface      = require('famous/core/Surface');
     var StateModifier = require('famous/modifiers/StateModifier');
-
     var ScrollItem   = require('components/ScrollListItem');
+
+    var ShelfView = require('views/ShelfView');
 
 
     require('famous/inputs/FastClick');
@@ -112,73 +113,7 @@ define(function(require, exports, module) {
     var scrollDemo = new ScrollDemo();
     var aboutPage  = new DetailView(aboutContent);
 
-    var getBooks = function(params, callback) {
-        reqwest({
-            url: 'https://betterreadsapi.azurewebsites.net/booksOnShelf',
-            method: 'get',
-            data: params,
-            success: callback
-        });
-    };
-
-
-    //custom view
-    var libraryView = new View();
-    getBooks({id: '4067289', shelf: 'to-read', page: 1, per_page: 50}, function(data) {
-        // var book = JSON.parse(data)[18];
-        var books = JSON.parse(data);
-        console.log(books);
-        //need to change image size so it is whatever aspect ratio of book is; there is slight variance around 100:150
-        // var image = new ImageSurface({
-        //     size: [100, 150]
-        // });
-        // image.setContent(book.image_url[0]);
-        // var imageModifier = new Modifier({
-        //     origin: [0.5, 0.5]
-        // });
-        // libraryView.add(imageModifier).add(image);
-        var scrollView = new ScrollView(this.options);
-        var listOfItems = [];
-
-        for (var i = 0; i < books.length; i++) {
-            // var listItem = new ScrollItem(i);
-
-            var bookView = new View();
-            var bookMod = new StateModifier({
-                size: [undefined, 150]
-            });
-
-            var tab = new Surface({
-              content: books[i].title[0] + '<br>' + books[i].authors[0].author[0].name[0] + '<br>Rating: ' + books[i].average_rating + '/5',
-              size: [undefined, undefined],
-              properties: {
-                textAlign: 'right',
-                backgroundColor: 'white'
-              }
-            });
-
-            var image = new ImageSurface({
-                size: [100, 150]
-            });
-            image.setContent(books[i].image_url[0]);
-
-            var bookWrapper = bookView.add(bookMod);
-            bookWrapper.add(image);
-            bookWrapper.add(tab);
-
-            listOfItems.push(bookView);
-            image.pipe(scrollView);
-            image.pipe(libraryView);
-        }
-
-        scrollView.sequenceFrom(listOfItems);
-        libraryView.add(scrollView);
-
-        libraryView._eventInput.on('scrollListItemClicked', function(eventPayload) {
-            this._eventOutput.emit('navigate:modal', eventPayload);
-        }.bind(libraryView));
-    });
-
+    var libraryView = new ShelfView();
 
     app
     .addPage({
