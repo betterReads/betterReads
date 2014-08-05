8d5b8aa8b1a7de95d9ca0567ddf29b7bfede9160
Austen Talbot <austentalbot@gmail.com>
2014-08-04 19:48:54 -0700

    Added book information to scroll view

--- www/src/main.js
+++ www/src/main.js
@@ -15,6 +15,8 @@ define(function(require, exports, module) {
     var View         = require('famous/core/View');
     var reqwest      = require('../../bower_components/reqwest/reqwest');
     var ScrollView   = require('famous/views/ScrollView');
+    var Surface      = require('famous/core/Surface');
+    var StateModifier = require('famous/modifiers/StateModifier');
 
     var ScrollItem   = require('components/ScrollListItem');
 
@@ -122,10 +124,10 @@ define(function(require, exports, module) {
 
     //custom view
     var libraryView = new View();
-    getBooks({id: '4067289', shelf: 'to-read', page: 1, per_page: 100}, function(data) {
+    getBooks({id: '4067289', shelf: 'to-read', page: 1, per_page: 50}, function(data) {
         // var book = JSON.parse(data)[18];
         var books = JSON.parse(data);
-        console.log(JSON.parse(data));
+        console.log(books);
         //need to change image size so it is whatever aspect ratio of book is; there is slight variance around 100:150
         // var image = new ImageSurface({
         //     size: [100, 150]
@@ -141,12 +143,30 @@ define(function(require, exports, module) {
         for (var i = 0; i < books.length; i++) {
             // var listItem = new ScrollItem(i);
 
+            var bookView = new View();
+            var bookMod = new StateModifier({
+                size: [undefined, 150]
+            });
+
+            var tab = new Surface({
+              content: books[i].title[0] + '<br>' + books[i].authors[0].author[0].name[0] + '<br>Rating: ' + books[i].average_rating + '/5',
+              size: [undefined, undefined],
+              properties: {
+                textAlign: 'right',
+                backgroundColor: 'white'
+              }
+            });
+
             var image = new ImageSurface({
                 size: [100, 150]
             });
             image.setContent(books[i].image_url[0]);
 
-            listOfItems.push(image);
+            var bookWrapper = bookView.add(bookMod);
+            bookWrapper.add(image);
+            bookWrapper.add(tab);
+
+            listOfItems.push(bookView);
             image.pipe(scrollView);
             image.pipe(libraryView);
         }
