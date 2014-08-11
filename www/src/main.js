ec801355d2289ba793a1336c073e96a754cbf986
Austen Talbot <austentalbot@gmail.com>
2014-08-11 11:28:13 -0700

    Restructured app so that pages load after log in

--- www/src/main.js
+++ www/src/main.js
@@ -119,72 +119,172 @@ define(function(require, exports, module) {
     }
   });
 
-  var famousView = new FamousView();
-  var scrollDemo = new ScrollDemo();
-  var aboutPage  = new DetailView(aboutContent);
+  var initializeApp = function() {
+    var app = new TabTemplate({
+      /*  
+      -----------------------------------------------------------------
+        The layout options for the App's HeaderFooterLayout which 
+        specify the app laid out vertically with pixel values for 
+        the height of the header and footer.  iOS retina displays
+        will double these pixel values on screen
+      -----------------------------------------------------------------
+      */
+      layoutOptions: {
+        direction: Utility.Direction.Y,
+        headerSize: 56,
+        footerSize: 60
+      },
 
-  var libraryView = new LibraryView('to-read');
-  var shelvesView = new ShelfView();
-  var friendsView = new FriendsView();
-  var searchView = new SearchView();
-  var logInView = new LogInView();
-  var bookView = new BookView();
-  var bestSellerView = new BestSellerView();
+      /*
+      -----------------------------------------------------------------
+        The ContentView constructors options.
+        Specify the default content background color
+      -----------------------------------------------------------------
+      */
+      content: {
+        backgroundColor: '#FFF',
+        famousView: {
+          rotationCoefficient: 0.003 
+        },
+        scrollDemo: {
+          edgeGrip: 0.8,
+          edgeDamp: 0.8,
+          clipSize: 800,
+          margin: 800
+        },
 
-  searchView.pipe(app.content);
-  searchView.pipe(bookView);
+      },
 
-  //set up shelf view rendering
-  shelvesView.on('shelfClick', function(shelf) {
-    var waitingView = new WaitingView();
-    var currShelf = new LibraryView(shelf, true);
-    app.addPage({
-      title: shelf,
-      renderable: currShelf
-    }).addPage({
-      title: 'Loading',
-      renderable: waitingView
-    }).showPage('Loading');
-    currShelf.on('shelfLoaded', function(shelf) {
-      app.showPage(shelf);
+      /*
+      -----------------------------------------------------------------
+        The FooterView contructor's options.
+        Specify the default Footer background color as well as other
+        options here.
+      -----------------------------------------------------------------
+      */
+      footer: {
+        backgroundColor: '#FFF'
+      },
+      
+      /*
+      -----------------------------------------------------------------
+        The HeaderView contructor's options.
+      -----------------------------------------------------------------
+      */
+      header: {
+        background  : {
+          classes : ['header-background']
+        },
+        layout         : {
+          direction  : Utility.Direction.X,
+          headerSize : 96,
+          footerSize : 96
+        },
+        title       : {
+          classes : ['header-title'],
+          content : 'Header'
+        },
+        leftButton    : {
+          classes   : ['header-left-button'],
+          content   : '<i class="fa fa-angle-left header-left-button-icon"></i><p class="header-left-button-label">Back</p>',
+          eventName : 'leftButtonClicked'
+        },
+        rightButton   : {
+          classes   : ['header-right-button'],
+          content   : '<i class="fa fa-angle-right header-right-button-icon"></i><p class="header-right-button-label">Forward</p>',
+          eventName : 'rightButtonClicked',
+        },
+        leftButtonRenderOptions : {
+          overlap             : false
+        },
+        rightButtonRenderOptions : {
+          overlap              : false
+        },
+        leftButtonShown  : false,
+        rightButtonShown : false
+      }
     });
-  });
 
+    var famousView = new FamousView();
+    var scrollDemo = new ScrollDemo();
+    var aboutPage  = new DetailView(aboutContent);
+
+    var libraryView = new LibraryView('to-read');
+    var shelvesView = new ShelfView();
+    var friendsView = new FriendsView();
+    var searchView = new SearchView();
+    var bookView = new BookView();
+    var bestSellerView = new BestSellerView();
+
+    searchView.pipe(app.content);
+    searchView.pipe(bookView);
+
+    //set up shelf view rendering
+    shelvesView.on('shelfClick', function(shelf) {
+      var waitingView = new WaitingView();
+      var currShelf = new LibraryView(shelf, true);
+      app.addPage({
+        title: shelf,
+        renderable: currShelf
+      }).addPage({
+        title: 'Loading',
+        renderable: waitingView
+      }).showPage('Loading');
+      currShelf.on('shelfLoaded', function(shelf) {
+        app.showPage(shelf);
+      });
+    });
+
+    app
+    .addPage({
+      title              : 'Library',
+      renderable         : libraryView,
+      footerIconName     : 'fa-book',
+      footerIconPosition : 0
+    }).addPage({
+      title              : 'Shelves',
+      renderable         : shelvesView,
+      footerIconPath     : 'resources/library.png',
+      footerIconPosition : 1
+    }).addPage({
+      title              : 'Friends',
+      renderable         : friendsView,
+      footerIconName     : 'fa-group  ',
+      footerIconPosition : 2
+    }).addPage({
+      title              : 'Search',
+      renderable         : searchView,
+      footerIconName     : 'fa-search',
+      footerIconPosition : 3
+    }).addPage({
+      title              : 'Book',
+      renderable         : bookView
+    }).showPage('Library');
+
+    var mainContext = Engine.createContext();
+    mainContext.add(app);
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    //http://plugins.cordova.io/#/package/org.apache.cordova.device
+    function onDeviceReady() {
+      // TO RUN ON DEVICE, INITIALIZE CONTEXT AND APP IN HERE
+    }
+  };
+
+
+  var logInView = new LogInView();
   logInView.on('loggedIn', function() {
     console.log('heard log in');
     //set up other views, etc
+    initializeApp();
+
   });
 
-  app
-  .addPage({
-    title              : 'Library',
-    renderable         : libraryView,
-    footerIconName     : 'fa-book',
-    footerIconPosition : 0
-  }).addPage({
-    title              : 'Shelves',
-    renderable         : shelvesView,
-    footerIconPath     : 'resources/library.png',
-    footerIconPosition : 1
-  }).addPage({
-    title              : 'Friends',
-    renderable         : friendsView,
-    footerIconName     : 'fa-group  ',
-    footerIconPosition : 2
-  }).addPage({
-    title              : 'Search',
-    renderable         : searchView,
-    footerIconName     : 'fa-search',
-    footerIconPosition : 3
-  }).addPage({
+  app.addPage({
     title              : 'Log In',
     renderable         : logInView,
-    footerIconName     : 'fa-exchange',
-    footerIconPosition : 4
-  }).addPage({
-    title              : 'Book',
-    renderable         : bookView
-  }).showPage('Library');
+  }).showPage('Log In');
 
   document.addEventListener("deviceready", onDeviceReady, false);
 
