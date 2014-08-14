e3900215e74b9c781bcb458f30891268ae1ac8d4
Austen Talbot <austentalbot@gmail.com>
2014-08-14 13:05:18 -0700

    Worked on improving friend update feed

--- www/src/views/FriendsView.js
+++ www/src/views/FriendsView.js
@@ -6,13 +6,14 @@ define(function(require, exports, module){
   var ImageSurface = require('famous/surfaces/ImageSurface');
   var StateModifier = require('famous/modifiers/StateModifier');
   var ScrollView = require('famous/views/ScrollView');
+  var Modifier = require('famous/core/Modifier');
+  var Transform = require('famous/core/Transform');
 
   var betterReads = require('../utils/BetterReads');
 
   function FriendsView(){
     View.apply(this, arguments);
 
-    // _addSurface.call(this);
     _addFeed.call(this);
   }
 
@@ -21,20 +22,6 @@ define(function(require, exports, module){
 
   FriendsView.DEFAULT_OPTIONS = {};
 
-  function _addSurface(){
-    var surface = new Surface({
-      content: "Friend Updates",
-      classes: ['preLoad'],
-      size: [undefined, undefined],
-      properties: {
-        textAlign: 'center',
-        lineHeight: '200px',
-        backgroundColor: '#bbb'
-      }
-    });
-
-    this.add(surface);
-  }
   function _addFeed(){
     var that = this;
     betterReads.getUpdates().then(function(results) {
@@ -46,16 +33,43 @@ define(function(require, exports, module){
       var colors = ['white', '#EFF9FF'];
       for (var i = 0; i < results.length; i++) {
         var update = results[i];
+
+        var bookView = new View();
+        var bookMod = new StateModifier({
+          size: [undefined, 100]
+        });
+
+        var image = new ImageSurface({
+          // properties: {
+          //   padding: '25px 0px px 0px'
+          // }
+        });
+        image.setContent(update.image_url[0]);
+
+        image.imageMod = new Modifier({
+          size: [50, undefined],
+          transform: Transform.translate(0, 0, 1)
+        });
+
         var tab = new Surface({
-          content: update.actor[0].name[0] + ' ' + update.action_text[0] + '<br>' + moment(update.updated_at[0]).format('MMMM Do YYYY, h:mm a'),
-          size: [undefined, true],
+          content: '<b>' + update.actor[0].name[0] + '</b> ' + update.action_text[0] + '<br>' + moment(update.updated_at[0]).format('MMMM Do YYYY, h:mm a'),
+          size: [undefined, 100],
           properties: {
             backgroundColor: colors[i%2],
-            padding: '10px'
+            padding: '10px 10px 10px 77px'
           }
         });
 
-        listOfItems.push(tab);
+
+        var bookWrapper = bookView.add(bookMod);
+        bookWrapper.add(image.imageMod).add(image);
+        bookWrapper.add(tab);
+
+        listOfItems.push(bookView);
+        image.pipe(scrollView);
+        image.pipe(that);
+
+        // listOfItems.push(tab);
         tab.pipe(scrollView);
         tab.pipe(that);
       }
